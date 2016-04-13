df33999c876542c19138278e68cc91892d709d4f
John J. Glynn IV <jglynn@factset.com>
2016-04-13 12:49:59 -0400

    added branch to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,5 @@
 
 git clone --bare "${ORIGIN:?}" "$HOME/.dotfiles"
 
-git --git-dir="$HOME/.dotfiles" --work-tree="$HOME" checkout
+git --git-dir="$HOME/.dotfiles" --work-tree="$HOME" checkout "${BRANCH:?}
 git --git-dir="$HOME/.dotfiles" --work-tree="$HOME" config status.showUntrackedFiles no
